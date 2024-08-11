22b878495d5407bad7abe2e824d7a531115479e4
pramodC135 <pramodmadhushan4@gmail.com>
2024-08-11 19:41:57 +0530

    add project and swagger

---
+++ Gatherly.App/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.Run();
+
